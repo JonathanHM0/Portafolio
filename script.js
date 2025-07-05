b851c7df23a57de0e1e665a1ef595de296ab87a9
Jonathan Hernandez <dbzjona1@gmail.com>
2025-07-04 18:12:07 -0600

    Subiendo portafolio inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+async function copiarAlPortapapeles() {
+  try {
+    const texto = document.getElementById("correo").innerText;
+    await navigator.clipboard.writeText(texto);
+    mostrarToast();
+    console.log("Contenido copiado al portapapeles");
+  } catch (err) {
+    console.error("Error al copiar: ", err);
+  }
+}
+
+function mostrarToast() {
+  const toast = document.getElementById("toast");
+  toast.classList.add("mostrar");
+
+  setTimeout(() => {
+    toast.classList.remove("mostrar");
+  }, 2500); // 2.5 segundos visible
+}
